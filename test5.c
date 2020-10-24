e1f225db75cb4dadce13c6943cadf9d96e59eb22
amyn96 <aminuddin.afiq@gmail.com>
2020-10-25 00:17:24 +0800

    LAB2

---
+++ test5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void childTask(){
+    char name[30];
+    printf("Enter name : ");
+    fgets(name, sizeof(name), stdin);
+    printf("\nMy name is ");
+    puts(name);
+    printf("My Child process => PPID=%d, PID=%d\n\n", getppid(), getpid()); 
+    exit(0);
+
+}
+
+void parentTask(){
+
+    printf("Parent process => PID=%d\n", getpid());
+    printf("All child process is finnised!\n");
+    printf("Job is done\n\n");
+
+}
+
+
+void waiting(){
+
+     printf("waiting for child process to finnish...");
+     wait(NULL);
+     printf("child process finnished!\n");
+     printf("_______________________________________________________________\n");
+
+
+}
+
+
+int main(void){
+    
+    for(int i = 1; i < 5; i++){
+	pid_t pid = fork();
+	if(pid == 0){
+	    childTask();
+	}else if(i == 4){
+	    waiting();
+            parentTask(); 
+	}else waiting();
+
+    }
+    return EXIT_SUCCESS;
+}
